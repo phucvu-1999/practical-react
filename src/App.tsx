f1814370e8ae924ab6790d352bcc6e55d1196146
Tien Phuc <leesinvn123@gmail.com>
2022-06-02 23:37:41 +0700

    [Learning/reactjs]: Learning react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,26 @@
-import { toast, ToastContainer, ToastTransition } from "react-toastify";
-import { AiFillGithub } from "react-icons/ai";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { useState } from "react";
+import Modal from "react-modal";
 
+Modal.setAppElement("#root");
 function App() {
-  const notify = () => {
-    toast("Hello world !!!", {
-      position: toast.POSITION.BOTTOM_LEFT,
-      autoClose: 10000,
-    });
-
-    toast.success("Hello world !!!", {
-      position: toast.POSITION.BOTTOM_RIGHT,
-      autoClose: false,
-    });
-
-    toast.error("Hello world !!!", {
-      position: toast.POSITION.TOP_RIGHT,
-    });
-
-    toast.info("Hello world !!!", {
-      position: toast.POSITION.TOP_LEFT,
-    });
-
-    toast.warn("Hello world !!!", {
-      position: toast.POSITION.BOTTOM_CENTER,
-    });
-
-    toast.info("Hello world !!!", {
-      position: toast.POSITION.TOP_CENTER,
-    });
-  };
+  const [isModalVisible, setModalIsVisible] = useState(false);
 
   return (
     <div className="App">
-      <button onClick={notify}>Show toastify</button>
-      <ToastContainer />
+      <Modal
+        onRequestClose={() => setModalIsVisible(!isModalVisible)}
+        isOpen={isModalVisible}
+      >
+        <h2>Hello world</h2>
+        <button onClick={() => setModalIsVisible(!isModalVisible)}>
+          Toggle Modal
+        </button>
+      </Modal>
+
+      <button onClick={() => setModalIsVisible(!isModalVisible)}>
+        Toggle Modal
+      </button>
     </div>
   );
 }
