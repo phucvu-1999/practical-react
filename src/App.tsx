43cc512f6bc323938d07696eefa365a2da4991af
Tien Phuc <leesinvn123@gmail.com>
2022-06-02 22:42:05 +0700

    [Learning/React]: Learnig react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import { BsAlarm, BsAlarmFill } from "react-icons/bs";
-import { IconContext } from "react-icons";
+import { toast, ToastContainer, ToastTransition } from "react-toastify";
+import { AiFillGithub } from "react-icons/ai";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
+  const notify = () => {
+    toast("Hello world !!!", {
+      position: toast.POSITION.BOTTOM_LEFT,
+      autoClose: 10000,
+    });
+
+    toast.success("Hello world !!!", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: false,
+    });
+
+    toast.error("Hello world !!!", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+
+    toast.info("Hello world !!!", {
+      position: toast.POSITION.TOP_LEFT,
+    });
+
+    toast.warn("Hello world !!!", {
+      position: toast.POSITION.BOTTOM_CENTER,
+    });
+
+    toast.info("Hello world !!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+
   return (
-    <IconContext.Provider value={{ size: "10rem" }}>
-      <div className="App">
-        <BsAlarm color="green" size="1.5rem" />
-        <BsAlarmFill />
-      </div>
-    </IconContext.Provider>
+    <div className="App">
+      <button onClick={notify}>Show toastify</button>
+      <ToastContainer />
+    </div>
   );
 }
 
