340c1a60e75ec7bfb693de37b64265026bbb27dd
Tien Phuc <leesinvn123@gmail.com>
2022-06-03 00:10:43 +0700

    [Learning/reactjs]: Learning react-tooltips

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
+import Tippy from "@tippy.js/react";
+import { forwardRef, LegacyRef } from "react";
+import "tippy.js/dist/tippy.css";
 import "./App.css";
-import { useState } from "react";
-import Modal from "react-modal";
 
-Modal.setAppElement("#root");
+const ColoredToolTip = () => {
+  return <span style={{ color: "yellow" }}>Yellow</span>;
+};
+
+const renderList = () => {
+  return (
+    <ul style={{ fontSize: "1rem" }}>
+      <li>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad totam
+        excepturi qui quas vel deserunt? Similique fuga quod cum. Facere
+        delectus est, quo dicta ipsum nobis consequatur mollitia corporis.
+        Sapiente?
+      </li>
+      <li>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad totam
+        excepturi qui quas vel deserunt? Similique fuga quod cum. Facere
+        delectus est, quo dicta ipsum nobis consequatur mollitia corporis.
+        Sapiente?
+      </li>
+      <li>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad totam
+        excepturi qui quas vel deserunt? Similique fuga quod cum. Facere
+        delectus est, quo dicta ipsum nobis consequatur mollitia corporis.
+        Sapiente?
+      </li>
+    </ul>
+  );
+};
+
 function App() {
-  const [isModalVisible, setModalIsVisible] = useState(false);
-
   return (
     <div className="App">
-      <Modal
-        onRequestClose={() => setModalIsVisible(!isModalVisible)}
-        isOpen={isModalVisible}
-      >
-        <h2>Hello world</h2>
-        <button onClick={() => setModalIsVisible(!isModalVisible)}>
-          Toggle Modal
-        </button>
-      </Modal>
+      <Tippy arrow={false} content={renderList()}>
+        <button>Hover Me !!!</button>
+      </Tippy>
 
-      <button onClick={() => setModalIsVisible(!isModalVisible)}>
-        Toggle Modal
-      </button>
+      <Tippy sticky duration={1000} content={<ColoredToolTip />}>
+        <p>Hi there</p>
+      </Tippy>
     </div>
   );
 }
